c3808cd5077d93a87f9ed6a5c719a0ee9324d0f1
freddy campos <fmcampos_new@hotmail.com>
2020-10-23 15:15:20 -0300

    correo de confirmacion

--- myProyectoCarW/myProyectoCarW/settings.py
+++ myProyectoCarW/myProyectoCarW/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'camp.fred.2020@gmail.com'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxx'
+EMAIL_BACKEND = 'django.core.mail.backends.smpt.EmailBackend'
+
